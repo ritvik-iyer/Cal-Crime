8c223986f17ca88b6480e4df2f6f0ff23f90e246
Ritvik Iyer <rikiiyer@gmail.com>
2019-10-05 14:28:05 -0700

    fix newline error

--- utils.py
+++ utils.py
@@ -2,6 +2,6 @@ import pandas as pd
 from sodapy import Socrata
 
 keys = open('api-key.txt').readlines()
-app_key, username, password = keys[0:2]
+app_key, username, password = [x.strip('\n') for x in keys[0:3]]
 
 client = Socrata("data.cityofberkeley.info", app_key, username=username, password=password)
